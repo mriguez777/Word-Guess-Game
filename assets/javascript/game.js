1ada1c9cf91587c1768bbd0a7e3fb5a72f5aafa9
MROD <mriguez777@gmail.com>
2020-02-18 23:42:29 -0700

    completed variables and functions on game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-var $ = fucntion (id) {
+var $ = function (id) {
     return document.getElementByID(id);
 }
 
@@ -10,16 +10,76 @@ var $ = fucntion (id) {
 var tarot = ["Fool", "Magician", "Emperor", "Chariot", "Hierophant", "Strength", "Justice", "Hanged Man", "Star", "Judgement"]
 
 //Next I setup the choice variable with a random number formula to generate with index to use. 
-var choice = Math.floor(Math.random()*10)
+var choice = Math.floor(Math.random() * 10)
 
 //Setting another variable that gives me the answer of the random index
 var answer = tarot[choice];
 
-//variable to saving the length of the answer in variable "myLength".
+//variable to saving the length of the answer in variable "myLength". If I understand this corretly it means PC treats this as a game of guessing the length the user treats it as guessing a word as set with the win variable.
 var myLength = answer.length;
 
 //variable to display length, we need this for underscore appearance.
-var display=[myLength];
+var display = [myLength];
 
-alert(answer);
+//in order to win they have to guess the correct length
+var win = myLength;
+
+//this is creating another array which stores the "tarot index" choosen and seperates each letter into another index
+var letters = answer.split('');
+
+//numer of attempts left 
+var attemptsLeft = 5;
+
+//this is allows us to hide the text
+var output = "";
+
+//what is obtained from the users input
+var userLetter = "";
+
+
+var setup = function () {
+    for (var i = 0; i < answer.length; i++) {
+        display[i] = "_";
+        output = output + display[i]
+    }
+    document.getElementById("tarot").innerHTML = output;
+    output = "";
+}
+
+var submit = function () {
+    output = "";
+    userLetter = $("letter").value;
+    $("letter").value = "";
+
+    for (var c = 0; c < answer.length; c++) {
+        alert(letters[c]);
+        if (userLetter.toUpperCase() == leters[c]) {
+            display[c] = userLetter.toUpperCase();
+            win--;
+        }
+        output = output + dislay[c] + " ";
+    }
+
+}
+
+document.getElementById("tarot").innerHTML = output;
+output = "";
+attemptsLeft--;
+
+if (win < 1) {
+    document.getElementById("guesses").innerHTML ="YOU WIN!"
+}
+else if (attemptsLeft < 1) {
+    document.getElementById("guesses").innerHTML ="YOU LOSE!"
+}
+else {
+    document.getElementById("guesses").innerHTML ="You have" + attemptsLeft + "guesses left";
+}
+
+window.onload = function() {
+    setup();
+    this.$("submit").onclick = submit;
+}
+
+
 
